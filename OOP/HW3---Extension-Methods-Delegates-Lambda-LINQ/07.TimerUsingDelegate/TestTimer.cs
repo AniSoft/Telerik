af44a014ea2695f98ecb0dacf6178756232320bd
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:02:12 +0300

    Create TestTimer.cs

---
+++ OOP/HW3---Extension-Methods-Delegates-Lambda-LINQ/07.TimerUsingDelegate/TestTimer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _07.TimerUsingDelegate
+{
+    class TestTimer
+    {
+        static void Main(string[] args)
+        {
+            TimerUsingDelegate t = new TimerUsingDelegate(TimerPrinting, 2);
+            Thread timerThread = new Thread(new ThreadStart(t.StartTimer));
+            timerThread.Start();
+            
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("number:" + i);
+            }
+            t.StopTimer();
+            Console.WriteLine("no timer");
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("number:" + i);
+            }
+        }
+
+        public static void TimerPrinting()
+        {
+            Console.WriteLine("I am delegate and print myself at some interval");
+        }
+    }
+}
