f0d5cb39e67539324bf9f1f72a0fa78930a48412
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:21:25 +0300

    Create MortgageAccount.cs

---
+++ OOP/HW5--OOP-Principles---Part-II/02.Bank/MortgageAccount.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Bank
+{
+    public class MortgageAccount : Account
+    {
+        // Constructor
+        public MortgageAccount(Customer customer, double monthInterestRate, double balance = 0)
+            : base(customer, monthInterestRate, balance)
+        {
+        }
+
+        public override double CalculateInterestAmount(int numberOfMonths)
+        {
+            if (this.customer is Person && numberOfMonths > 6)
+            {
+                return (numberOfMonths - 6) * MonthInterestRate;
+            }
+            if (this.customer is Company)
+            {
+                if (numberOfMonths > 12)
+                {
+                    return numberOfMonths * MonthInterestRate;
+                }
+                else
+                {
+                    return 0.5 * numberOfMonths * MonthInterestRate;
+                }
+            }
+            return 0;
+        }
+    }
+}
