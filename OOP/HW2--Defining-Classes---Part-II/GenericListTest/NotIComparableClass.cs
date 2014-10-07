b6d342c9498dbec6dca969665362d722eb202cc5
Anita <AniSoft@users.noreply.github.com>
2014-10-07 17:08:31 +0300

    Update NotIComparableClass.cs

--- OOP/HW2--Defining-Classes---Part-II/GenericListTest/NotIComparableClass.cs
+++ OOP/HW2--Defining-Classes---Part-II/GenericListTest/NotIComparableClass.cs
@@ -1,15 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace GenericListTest
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     class NotIComparableClass
     {
         private int number;
         public int Number { get; set; }
+
         public NotIComparableClass(int num)
         {
             this.Number = num;
