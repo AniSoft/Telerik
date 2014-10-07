44ff4dd76e113c5ac013548a9f15a36bd8d44ac9
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:06:00 +0300

    Create Discipline.cs

---
+++ OOP/HW4--OOP-Principles---Part-I/01.School/Discipline.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.School
+{
+    public class Discipline : ICommentable
+    {
+        private int lectureNumbers;
+        private int exerciseNumbers;
+
+        public string Comment { get; set; }
+        public string Name { get; set; }
+        public int LectureNumbers
+        {
+            get { return this.lectureNumbers; }
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException("lectureNumbers must be larger than 0");
+                }
+                else
+                {
+                    this.lectureNumbers = value;
+                }
+            }
+        }
+        public int ExerciseNumbers
+        {
+            get { return this.exerciseNumbers; }
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException("exerciseNumbers must be larger than 0");
+                }
+                else
+                {
+                    this.exerciseNumbers = value;
+                }
+            }
+        }
+
+
+        // Constructor
+        public Discipline(string name, int lectureNumbers, int exerciseNumbers)
+        {
+            this.Name = name;
+            this.LectureNumbers = lectureNumbers;
+            this.ExerciseNumbers = exerciseNumbers;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - L{1}, E{2}", this.Name, this.LectureNumbers, this.ExerciseNumbers);
+        }
+    }
+}
