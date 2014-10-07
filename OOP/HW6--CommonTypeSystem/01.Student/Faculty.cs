e5044ff8324e91f9c428e157ac1395b3577d431d
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:25:16 +0300

    Create Faculty.cs

---
+++ OOP/HW6--CommonTypeSystem/01.Student/Faculty.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Student
+{
+    public enum Faculty
+    {
+        None,
+        Engineer,
+        Finance,
+        StructuralEngineer,
+        Biology
+    }
+}
