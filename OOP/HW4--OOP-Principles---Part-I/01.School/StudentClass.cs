42c5ffbfb3773cdccc1377f2a9fbfc0768407765
Anita <AniSoft@users.noreply.github.com>
2014-10-07 17:25:16 +0300

    Update StudentClass.cs

--- OOP/HW4--OOP-Principles---Part-I/01.School/StudentClass.cs
+++ OOP/HW4--OOP-Principles---Part-I/01.School/StudentClass.cs
@@ -1,16 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace _01.School
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class StudentClass : ICommentable
     {
         public List<Student> StudentSet { get; set; }
+
         public List<Teacher> TeacherSet { get; set; }
+
         public string TextIdentifier { get; set; }
+
         public string Comment { get; set; }
 
         // Constructor
@@ -20,6 +23,7 @@ namespace _01.School
             this.StudentSet = new List<Student>();
             this.TeacherSet = new List<Teacher>();
         }
+
         public StudentClass(string textIdentifier, List<Student> studentSet, List<Teacher> teacherSet)
         {
             this.TextIdentifier = textIdentifier;
@@ -32,17 +36,20 @@ namespace _01.School
             StringBuilder sb = new StringBuilder();
             sb.Append(String.Format("Class '{0}'",TextIdentifier));
             sb.Append("\nSet of students:\n");
+
             foreach (var item in StudentSet)
             {
                 sb.Append(item + ";\n");
             }
             sb.Remove(sb.Length - 3, 3);
             sb.Append("\nSet of teachers:\n");
+
             foreach (var item in TeacherSet)
             {
                 sb.Append(item + ";\n");
             }
             sb.Remove(sb.Length - 3, 3);
+
             return sb.ToString();
         }
     }
