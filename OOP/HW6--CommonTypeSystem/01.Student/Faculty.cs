163b4032be3696cd6e94157fd554d327bd280bb1
Anita <AniSoft@users.noreply.github.com>
2014-10-07 17:38:31 +0300

    Update Faculty.cs

--- OOP/HW6--CommonTypeSystem/01.Student/Faculty.cs
+++ OOP/HW6--CommonTypeSystem/01.Student/Faculty.cs
@@ -1,11 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace _01.Student
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public enum Faculty
     {
         None,
