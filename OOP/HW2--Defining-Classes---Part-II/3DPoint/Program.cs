f8c9e18c104d4a6e65b617b049b11737b6ebb4e7
Anita <AniSoft@users.noreply.github.com>
2014-10-07 14:39:43 +0300

    Update Program.cs

--- OOP/HW2--Defining-Classes---Part-II/3DPoint/Program.cs
+++ OOP/HW2--Defining-Classes---Part-II/3DPoint/Program.cs
@@ -1,11 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace _3DPoint
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     class Program
     {
         static void Main()
