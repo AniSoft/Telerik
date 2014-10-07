06a26476f9246a71a2bd9d1f7d73ffd846705f5c
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:03:59 +0300

    Create TimerClass.cs

---
+++ OOP/HW3---Extension-Methods-Delegates-Lambda-LINQ/08.TimerClass/TimerClass.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _07.TimerUsingDelegate
+{
+    
+    public class TimerClass
+    {
+        // The event that will be raised when time elapses
+        
+        public event TimeElapsedEventHandler TimeElapsed;
+        private int interval = 0;
+        private bool stopper = true;
+
+        public int Interval
+        {
+            get
+            {
+                return interval;
+            }
+            set
+            {
+                interval = value;
+            }
+        }
+
+        protected void OnTimeElapsed(int counter)
+        {
+            if (TimeElapsed != null)
+            {
+                TimeElapsedEventArgs e = new TimeElapsedEventArgs(counter);
+                TimeElapsed(this, e);
+            }
+        }
+
+        public TimerClass(int interval)
+        {
+            this.interval = interval;
+        }
+        public void StartTimer()
+        {
+            int counter = 0;
+            stopper = false;
+            while (stopper == false)
+            {
+                Thread.Sleep(this.interval);
+                if (stopper == false)
+                {
+                    OnTimeElapsed(counter);
+                    counter++;
+                }
+            }
+
+
+        }
+        public void StopTimer()
+        {
+            stopper = true;
+        }
+    }
+}
