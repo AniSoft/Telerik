bf41945a4671c83994c9106994eb8c273416206f
Anita <AniSoft@users.noreply.github.com>
2014-10-07 12:44:57 +0300

    Create DistanceIn3D.cs

---
+++ OOP/HW2--Defining-Classes---Part-II/3DPoint/DistanceIn3D.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DPoint
+{
+    class DistanceIn3D
+    {
+        private Point3D firstPoint;
+        private Point3D secondPoint;
+
+        public Point3D FirstPoint
+        {
+            get { return firstPoint; }
+            set { firstPoint = value; }
+        }
+        public Point3D SecondPoint
+        {
+            get { return secondPoint; }
+            set { secondPoint = value; }
+        }
+
+        public double CalcDistance(Point3D firstPoint, Point3D secondPoint)
+        {
+            return Math.Round(Math.Sqrt(Math.Pow(secondPoint.X - firstPoint.X,2) + Math.Pow(secondPoint.Y - firstPoint.Y,2) + Math.Pow(secondPoint.Z - firstPoint.Z,2)),2);
+        }
+        
+    }
+}
