ce037eca55831be98b0baca9507ba0f3c559052e
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:09:20 +0300

    Create Human.cs

---
+++ OOP/HW4--OOP-Principles---Part-I/02.HumanClasses/Human.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.HumanClasses
+{
+    public abstract class Human
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
