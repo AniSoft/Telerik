8cc7340d19efb1fdd75add9ed9512abbcc596633
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:17:33 +0300

    Create Triangle.cs

---
+++ OOP/HW5--OOP-Principles---Part-II/01.ShapeClass/Triangle.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ShapeClass
+{
+    public class Triangle : Shape
+    {
+        //Constructor
+        public Triangle(double width, double height)
+            : base(width, height)
+        {
+        }
+        public override double CalculateSurface()
+        {
+            return this.Width * this.Height / 2;
+        }
+    }
+}
