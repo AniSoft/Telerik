7c228a03a03828633236a4fc13b6a67b6b789820
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:18:40 +0300

    Create Account.cs

---
+++ OOP/HW5--OOP-Principles---Part-II/02.Bank/Account.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Bank
+{
+    public abstract class Account : ICalculetable
+    {
+        private double balance;
+        private double monthInterestRate;
+        public Customer customer { get; protected set; }
+
+        public double Balance
+        {
+            get { return balance; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Account balance must be positive number or zero");
+                }
+                else
+                {
+                    balance = value;
+                }
+            }
+        }
+        public double MonthInterestRate
+        {
+            get { return monthInterestRate; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Account balance must be positive number or zero");
+                }
+                else
+                {
+                    monthInterestRate = value;
+                }
+            }
+        }
+
+        // Constructor
+        public Account(Customer customer, double monthInterestRate, double balance = 0)
+        {
+            this.customer = customer;
+            this.MonthInterestRate = monthInterestRate;
+            this.Balance = balance;
+        }
+
+        public virtual void DepositeMoney(double money)
+        {
+            if (money <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Deposited money must be positive number");
+            }
+            else
+            {
+                Balance += money;
+            }
+        }
+
+        public virtual double CalculateInterestAmount(int numberOfMonths)
+        {
+            return numberOfMonths * MonthInterestRate;
+        }
+    }
+}
