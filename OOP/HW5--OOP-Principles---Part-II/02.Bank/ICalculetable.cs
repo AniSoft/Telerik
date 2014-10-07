746f74872d243c7887f04647b96a087074a23d37
Anita <AniSoft@users.noreply.github.com>
2014-10-07 17:33:15 +0300

    Update ICalculetable.cs

--- OOP/HW5--OOP-Principles---Part-II/02.Bank/ICalculetable.cs
+++ OOP/HW5--OOP-Principles---Part-II/02.Bank/ICalculetable.cs
@@ -1,11 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace _02.Bank
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     interface ICalculetable
     {
         double CalculateInterestAmount(int numberOfMonths);
