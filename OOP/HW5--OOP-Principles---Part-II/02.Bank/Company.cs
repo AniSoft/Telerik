9a49c2b767a64f4632d03bdf319554df9c9ad200
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:18:57 +0300

    Create Company.cs

---
+++ OOP/HW5--OOP-Principles---Part-II/02.Bank/Company.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Bank
+{
+    public class Company : Customer
+    {
+        private int companyID;
+        public int CompanyID
+        {
+            get { return companyID; }
+            set
+            {
+                if (value.ToString().Length != 8)
+                {
+                    throw new ArgumentOutOfRangeException("Personal ID must be 8 digits");
+                }
+                else
+                {
+                    companyID = value;
+                }
+            }
+        }
+
+        public Company(string name, string address, int companyID)
+            :base(name, address)
+        {
+            this.CompanyID = companyID;
+        }
+    }
+}
