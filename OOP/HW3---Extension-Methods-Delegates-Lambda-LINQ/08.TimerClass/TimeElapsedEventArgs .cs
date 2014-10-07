8a93c1c4e3228d33a8e8eaa49bf84abd3689f2a6
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:03:37 +0300

    Create TimeElapsedEventArgs .cs

---
+++ OOP/HW3---Extension-Methods-Delegates-Lambda-LINQ/08.TimerClass/TimeElapsedEventArgs .cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.TimerUsingDelegate
+{
+    // A delegate which processes the TimeElapsed event.
+    public delegate void TimeElapsedEventHandler(object sender, TimeElapsedEventArgs e);
+
+    /// A class which inherits System.EventArgs and keeps information for the ticks left
+    public class TimeElapsedEventArgs : EventArgs
+    {
+        private int ticksLeft;
+
+        public int TicksLeft
+        {
+            get
+            {
+                return this.ticksLeft;
+            }
+        }
+
+        public TimeElapsedEventArgs(int ticksLeft)
+        {
+            this.ticksLeft = ticksLeft;
+        }
+    }
+}
