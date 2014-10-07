9e6e62bd0b096a20699e1fb9234f18368b990822
Anita <AniSoft@users.noreply.github.com>
2014-10-07 12:52:41 +0300

    Create VersionAttribute.cs

---
+++ OOP/HW2--Defining-Classes---Part-II/VersionAttribute/VersionAttribute.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionAttribute
+{    
+    [AttributeUsage(AttributeTargets.Struct |
+        AttributeTargets.Class | 
+        AttributeTargets.Interface |
+        AttributeTargets.Enum |
+        AttributeTargets.Method, 
+        AllowMultiple = false)]
+    public class VersionAttribute : System.Attribute
+    {
+        public int MajorVersion { get; private set; }
+        public int MinorVersion { get; private set; }
+        public string Coment { get; private set; }
+
+        public VersionAttribute(int major, int minor, string comment)
+        {
+            this.MajorVersion = major;
+            this.MinorVersion = minor;
+            this.Coment = comment;
+        }
+        public VersionAttribute(int major, int minor) : this(major, minor, String.Empty)
+        {            
+        }
+    }
+}
