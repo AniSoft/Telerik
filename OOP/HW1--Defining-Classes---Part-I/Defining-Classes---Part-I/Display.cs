85079df153ce6e8663c941659e5dc9f1fac1746c
Anita <AniSoft@users.noreply.github.com>
2014-10-07 12:35:11 +0300

    Create Display.cs

---
+++ OOP/HW1--Defining-Classes---Part-I/Defining-Classes---Part-I/Display.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defining_Classes___Part_I
+{
+    public class Display
+    {
+        // Fields
+        private double size;
+        private int numberofColors;
+
+        //Properties
+        public double Size
+        {
+            get { return this.size; }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.size = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("size","Size can't be negative!");
+                }
+            }
+        }
+        public int NumberofColors
+        {
+            get { return this.numberofColors; }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.numberofColors = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("numberofColors","NumberofColors can't be negative!");
+                }
+            }
+        }
+
+        //Constructors
+        public Display()
+            : this(0, 0)   // Reuse the down constructor
+        {
+        }
+        public Display(int size, int numberofColors)
+        {
+            this.Size = size;
+            this.NumberofColors = numberofColors;
+        }
+
+    }
+}
