a62730508d851d508373306d0429ca4ed1ee7d43
Anita <AniSoft@users.noreply.github.com>
2014-10-07 12:33:55 +0300

    Create Program.cs

---
+++ OOP/HW1--Defining-Classes---Part-I/Defining-Classes---Part-I/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defining_Classes___Part_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
