a83a1000487fc1fc06856d4f0f4dc5b0b01d3b81
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:26:29 +0300

    Create University.cs

---
+++ OOP/HW6--CommonTypeSystem/01.Student/University.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Student
+{
+    public enum University
+    {
+        None,
+        TU,
+        SU,
+        UASG,
+        UNSS
+    }
+}
