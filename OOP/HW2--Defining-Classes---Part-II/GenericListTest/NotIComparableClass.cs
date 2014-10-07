c0122673bca92c8d8052738daf1f6d5f3d3a7584
Anita <AniSoft@users.noreply.github.com>
2014-10-07 12:49:06 +0300

    Create NotIComparableClass.cs

---
+++ OOP/HW2--Defining-Classes---Part-II/GenericListTest/NotIComparableClass.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericListTest
+{
+    class NotIComparableClass
+    {
+        private int number;
+        public int Number { get; set; }
+        public NotIComparableClass(int num)
+        {
+            this.Number = num;
+        }
+    }
+}
