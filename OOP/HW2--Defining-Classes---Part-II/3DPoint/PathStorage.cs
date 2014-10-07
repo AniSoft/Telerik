d0ab155c94ca51fc78e4e36dc7765bcd397c7865
Anita <AniSoft@users.noreply.github.com>
2014-10-07 12:46:16 +0300

    Create PathStorage.cs

---
+++ OOP/HW2--Defining-Classes---Part-II/3DPoint/PathStorage.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace _3DPoint
+{
+    public static class PathStorage
+    {
+        private static List<Path> pathList;
+
+        static PathStorage()
+        {
+            pathList = new List<Path>();
+        }
+
+        public static void SaveToFile(List<Path> pathList, string filepath)
+        {
+            StreamWriter sw = new StreamWriter(filepath);
+            StringBuilder sb = new StringBuilder();
+            foreach (var path in pathList)
+            {
+                foreach (var item in path.Pathlist)
+                {
+                    sb.Append(String.Format("{0},{1},{2}|", item.X, item.Y, item.Z));
+                }
+                sw.WriteLine(sb.ToString());
+                sb.Clear();
+            }
+            sw.Close();
+        }
+
+        public static List<Path> LoadFromFile(string filepath)
+        {
+            StreamReader sr = new StreamReader(filepath);
+            string row = sr.ReadLine();
+            Point3D point = new Point3D();
+            
+            while (row != null)
+            {
+                Path path = new Path();
+                string[] paths = row.Split(new char[]{'|'}, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var item in paths)
+                {
+                    double x, y, z;
+                    string[] coordinates = item.Split(',');
+                    if (coordinates.Length == 3 && double.TryParse(coordinates[0], out x)
+                        && double.TryParse(coordinates[1], out y) && double.TryParse(coordinates[2], out z))
+                    {
+                        point.X = x;
+                        point.Y = y;
+                        point.Z = z;
+                        path.Add(point);
+                    }
+                }
+                pathList.Add(path);
+                //path.Clear();
+
+                row = sr.ReadLine();
+            }
+            return pathList;
+        }
+    }
+}
