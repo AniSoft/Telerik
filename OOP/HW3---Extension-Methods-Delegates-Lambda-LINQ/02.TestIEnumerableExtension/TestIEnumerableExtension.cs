028ad302ec40942410306d9b57120533df8c3000
Anita <AniSoft@users.noreply.github.com>
2014-10-07 17:15:53 +0300

    Update TestIEnumerableExtension.cs

--- OOP/HW3---Extension-Methods-Delegates-Lambda-LINQ/02.TestIEnumerableExtension/TestIEnumerableExtension.cs
+++ OOP/HW3---Extension-Methods-Delegates-Lambda-LINQ/02.TestIEnumerableExtension/TestIEnumerableExtension.cs
@@ -1,23 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using _02.IEnumerableExtensions;
-
 namespace _02.TestIEnumerableExtension
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using _02.IEnumerableExtensions;
+    
     class TestIEnumerableExtension
     {
         static void Main()
         {
             int[] arr = new int[] { 1, 5, 67, 88, 56, 24, 3, -43, 1, -5, 34, 4 };
+
             Console.WriteLine(arr.Sum());
             Console.WriteLine(arr.Product());
             Console.WriteLine(arr.Min());
             Console.WriteLine(arr.Max());
             Console.WriteLine(arr.Average());
+
             var human = new Human(1.56, "Ivan", 36);
+
             List<Human> humanList = new List<Human>()
             {
                 new Human(1.86,"Ivan",26),
@@ -27,7 +30,8 @@ namespace _02.TestIEnumerableExtension
                 new Human(1.84,"Stoyan",19)
             };
             
-            Console.WriteLine(humanList.Average(new CustomTypeNumericalValue<Human>(Human.GetHeight)));  // Method in delegate is set without parametars
+             // Method in delegate is set without parametars
+            Console.WriteLine(humanList.Average(new CustomTypeNumericalValue<Human>(Human.GetHeight))); 
 
             Console.WriteLine("Inline c# function");
             Console.WriteLine(humanList.Average(x => x.Height));
@@ -37,7 +41,9 @@ namespace _02.TestIEnumerableExtension
     public class Human :IComparable
     {
         public double Height { get; set; }
+
         public string Name { get; set; }
+
         public int Age { get; set; }
 
         public Human(double height, string name, int age)
@@ -46,17 +52,18 @@ namespace _02.TestIEnumerableExtension
             this.Name = name;
             this.Age = age;
         }
-        public static decimal GetHeight(Human h) // must be static
+        
+        // must be static
+        public static decimal GetHeight(Human h) 
         {
             return (decimal)h.Height;
         }
-
-        public int CompareTo(object obj)  // compare method must get object type
+        
+        // compare method must get object type
+        public int CompareTo(object obj)  
         {
             Human h = (Human)obj;
             return this.Height.CompareTo(h.Height);
         }
-
-
     }
 }
