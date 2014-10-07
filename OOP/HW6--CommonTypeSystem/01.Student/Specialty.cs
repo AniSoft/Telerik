8555c95d2ffeb8309e810cccf37dbc9e27780ad3
Anita <AniSoft@users.noreply.github.com>
2014-10-07 17:39:33 +0300

    Update Specialty.cs

--- OOP/HW6--CommonTypeSystem/01.Student/Specialty.cs
+++ OOP/HW6--CommonTypeSystem/01.Student/Specialty.cs
@@ -1,11 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace _01.Student
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public enum Specialty
     {
         None,
