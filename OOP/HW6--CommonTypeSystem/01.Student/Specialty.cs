ffee1035bcf5fcd1cd517b452500bdd217cd3def
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:25:34 +0300

    Create Specialty.cs

---
+++ OOP/HW6--CommonTypeSystem/01.Student/Specialty.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Student
+{
+    public enum Specialty
+    {
+        None,
+        SoftwareEngineer,
+        Mathematic,
+        Biology,
+        Finance,
+        History,
+        CivilEngineer,
+        ASPEngineer
+    }
+}
