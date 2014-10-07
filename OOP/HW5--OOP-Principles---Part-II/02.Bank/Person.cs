16c6b434f17e3604bf1a161ffe171ddfe3dd3e00
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:21:42 +0300

    Create Person.cs

---
+++ OOP/HW5--OOP-Principles---Part-II/02.Bank/Person.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Bank
+{
+    public class Person : Customer
+    {
+        private long personalID;
+        public long PersonalID
+        {
+            get { return personalID; }
+            set
+            {
+                if (value.ToString().Length != 10)
+                {
+                    throw new ArgumentOutOfRangeException("Personal ID must be 10 digits");
+                }
+                else
+                {
+                    personalID = value;
+                }
+            }
+        }
+
+        public Person(string name, string address, long personalID)
+            :base(name, address)
+        {
+            this.PersonalID = personalID;
+        }
+    }
+}
