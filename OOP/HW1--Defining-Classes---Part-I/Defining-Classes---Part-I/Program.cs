a7791b3dfd3c7b8048fc68c3e6f91f02d5230dcd
Anita <AniSoft@users.noreply.github.com>
2014-10-07 14:33:41 +0300

    Update Program.cs

--- OOP/HW1--Defining-Classes---Part-I/Defining-Classes---Part-I/Program.cs
+++ OOP/HW1--Defining-Classes---Part-I/Defining-Classes---Part-I/Program.cs
@@ -1,14 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Defining_Classes___Part_I
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             
         }
