e0c6ae88b6c8c5e25b55e10a5823cb84032e7c22
Anita <AniSoft@users.noreply.github.com>
2014-10-07 17:35:58 +0300

    Update InvalidRangeException.cs

--- OOP/HW5--OOP-Principles---Part-II/03.InvalidRangeException/InvalidRangeException.cs
+++ OOP/HW5--OOP-Principles---Part-II/03.InvalidRangeException/InvalidRangeException.cs
@@ -1,11 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace _03.InvalidRangeException
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class InvalidRangeException<T> : Exception
         where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
     {
@@ -18,19 +18,19 @@ namespace _03.InvalidRangeException
             this.StartRange = startRange;
             this.EndRange = endRange;
         }
+
         public InvalidRangeException(T startRange, T endRange, string message)
             :base(message)
         {
             this.StartRange = startRange;
             this.EndRange = endRange;
         }
+
         public InvalidRangeException(T startRange, T endRange, string message, Exception innerException)
             : base(message, innerException)
         {
             this.StartRange = startRange;
             this.EndRange = endRange;
         }
- 
-        
     }
 }
