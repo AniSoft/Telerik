5d074f1dc8066d269229ef022b397c22fe54eaa6
Anita <AniSoft@users.noreply.github.com>
2014-10-07 13:19:18 +0300

    Create Customer.cs

---
+++ OOP/HW5--OOP-Principles---Part-II/02.Bank/Customer.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Bank
+{
+    public abstract class Customer
+    {
+        private string name;
+        private string address;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+                name = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Address cannot be null or empty.");
+                }
+                address = value;
+            }
+        }
+
+        public Customer(string name, string address)
+        {
+            this.Name = name;
+            this.Address = address;
+        }
+    }
+}
